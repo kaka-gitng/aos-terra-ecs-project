38d04feda9090486ff31333023d5ee4918aebe9a
kaka-gitng <kpnayoo@gmail.com>
2023-06-22 15:36:11 -0400

    another

--- alb.tf
+++ alb.tf
@@ -34,7 +34,7 @@ resource "aws_lb_target_group" "alb_target_group" {
 
 # create a listener on port 80 with redirect action
 resource "aws_lb_listener" "alb_http_listener" {
-  load_balancer_arn = aws_lb.application_load_balance.arn
+  load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = 80
   protocol          = "HTTP"
 
